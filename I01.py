0a91bb35dfe37a84c781675f35cf587857b7e0ad
Jeongseop Lee <wanderkind@semyung.ac.kr>
2021-09-12 16:06:41 +0900

    Update I01.py

--- I01.py
+++ I01.py
@@ -196,11 +196,10 @@ while True:
                 lxx[i] = str((int(lxx[i]) + int(lxx[(i + p[20 + u])%int(6*N/12)]))%10)
             lxx[i] = lxx[(i - (int(D(10))%24))%G]
         
-        X = (int(D(11)[8] + D(12)[19]) + 5*i)%16
-        W = str(bin(X)[2:]).zfill(4)
-        
         cq =[]
         for i in range(int(6*N/4)):
+            X = (int(D(11)[8] + D(12)[19]) + 5*i)%16
+            W = str(bin(X)[2:]).zfill(4)
             cq.append((int(str(lxx[4*i + int(W[0])]) + str(lxx[4*i + int(W[1]) + 1]) + str(lxx[4*i + int(W[2]) + 2]) + str(lxx[4*i + int(W[3]) + 3])) + int(D(13))%((27*i + 7)%(p[i%24]*p[(i+11)%24]) + 3))%24)
         
         a1 = []
